3f6350c39b91dcbe4e71c3c8e28fe6514ccb9642
shivam Singh Yadav <shivamsinghyadavssy888@gmail.com>
2025-02-24 08:49:18 +0530

    message to read

--- assesment_feedback/src/context/Context.jsx
+++ assesment_feedback/src/context/Context.jsx
@@ -12,16 +12,16 @@ const StudentProvider = ({ children }) => {
         setLoading(true);
     
         try {
-          const storedData = localStorage.getItem("studentsData");
-          // console.log("Stored Data:", storedData.length);
+          // const storedData = localStorage.getItem("studentsData");
+          // // console.log("Stored Data:", storedData.length);
           
-          if ( storedData && storedData.length > 0 ) {
-            console.log("Data from local storage:", JSON.parse(storedData));
-            setStudentData(JSON.parse(storedData));
-            return; // Exit function early
-          }
+          // if ( storedData && storedData.length > 0 ) {
+          //   console.log("Data from local storage:", JSON.parse(storedData));
+          //   setStudentData(JSON.parse(storedData));
+          //   return; // Exit function early
+          // }
     
-          console.log(import.meta.env.VITE_API_URL);
+          // console.log(import.meta.env.VITE_API_URL);
     
           const res = await fetch(`${import.meta.env.VITE_API_URL}/data`);
           if (!res.ok) throw new Error("Failed to fetch data");
